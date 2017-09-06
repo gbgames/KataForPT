09acaf3e0000656a4437c8abd5685b57d25bd88c
Gianfranco Berardi <gberardi@gbgames.com>
2017-09-06 00:09:26 -0500

    Implement demo to show off feature.

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,15 @@
 
+#include "CoinAccepter.h"
+#include "VendingMachineApp.h"
+#include "VendingMachineDisplay.h"
+#include <iostream>
+
 int main(int argc, char** argv)
 {
+	CoinAccepter accepter;
+	VendingMachineDisplay display(accepter);
+	VendingMachineApp app(std::cout, accepter, display);
+	app.run(std::cin);
+
 	return 0;
 }
