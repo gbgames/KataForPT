30c15e8decd199ae6bb27817cf2d2f3c61206f5d
Gianfranco Berardi <gberardi@gbgames.com>
2017-09-05 23:06:17 -0500

    Add VendingMachineApp implementation to add quarter based on input.

---
+++ source/proj/VendingMachineApp.cpp
@@ -0,0 +1,50 @@
+/*
+    Copyright 2017 Gianfranco Berardi. 
+ 	
+	This file is part of the KataForPT.
+
+    The KataForPT is free software: 
+	you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    KataForPT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with KataForPT.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+#include "VendingMachineApp.h"
+#include "CoinCandidate.h"
+#include "CoinAccepter.h"
+#include "VendingMachineDisplay.h"
+
+VendingMachineApp::VendingMachineApp(std::ostream & output, CoinAccepter & accepter, VendingMachineDisplay & display) : m_output(output), m_accepter(accepter), m_display(display)
+{
+}
+
+VendingMachineApp::~VendingMachineApp()
+{
+}
+
+void VendingMachineApp::run(std::istream & input)
+{
+	char choice;
+	do 
+	{
+		process(choice);
+		m_output << m_display.ui() << std::endl;
+	} while (input >> choice);
+}
+
+void VendingMachineApp::process(char choice)
+{
+	if ('q' == choice)
+	{
+		m_accepter.add(CoinCandidate(QUARTER_WEIGHT, QUARTER_DIAMETER, QUARTER_THICKNESS));
+	}
+}
---
+++ source/proj/VendingMachineApp.h
@@ -0,0 +1,43 @@
+/*
+    Copyright 2017 Gianfranco Berardi. 
+ 	
+	This file is part of the KataForPT.
+
+    The KataForPT is free software: 
+	you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    KataForPT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with KataForPT.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+#ifndef GB_VENDING_MACHINE_APP_H
+#define GB_VENDING_MACHINE_APP_H
+
+#include <iostream>
+
+class CoinAccepter;
+class VendingMachineDisplay;
+
+class VendingMachineApp
+{
+	public:
+		VendingMachineApp(std::ostream & output, CoinAccepter & accepter, VendingMachineDisplay & display);
+		~VendingMachineApp();
+
+		void run(std::istream & input);
+
+	private:
+		void process(char choice);
+		std::ostream & m_output;
+		CoinAccepter & m_accepter;
+		VendingMachineDisplay & m_display;
+};
+
+#endif
