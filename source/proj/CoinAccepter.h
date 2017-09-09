26aca52029a6236bc58e839effd397a5269fc9ee
Gianfranco Berardi <gberardi@gbgames.com>
2017-09-09 17:06:48 -0500

    Refactor to make makeChange() private.

--- source/proj/CoinAccepter.h
+++ source/proj/CoinAccepter.h
@@ -35,10 +35,10 @@ class CoinAccepter
 		void add(const CoinCandidate & candidate);
 		std::vector<CoinCandidate> returnedCoins() const;
 		void purchaseWith(Cents amount);
-		void makeChange(Cents amount);
 
 	private:
 		int m_currentAmount;
+		void makeChange(Cents amount);
 		std::vector<CoinCandidate> m_returnedCoins;
 };
 
