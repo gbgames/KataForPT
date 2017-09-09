ca80592e975fe8d15cd8c550d5c50887d7076ed5
Gianfranco Berardi <gberardi@gbgames.com>
2017-09-09 16:35:26 -0500

    Return nickel as change for 5 cents.

--- source/proj/CoinAccepter.cpp
+++ source/proj/CoinAccepter.cpp
@@ -76,4 +76,8 @@ void CoinAccepter::makeChange(Cents amount)
 	{
 		m_returnedCoins.push_back(CoinCandidate(DIME_WEIGHT, DIME_DIAMETER, DIME_THICKNESS));
 	}
+	else if (5 == amount)
+	{
+		m_returnedCoins.push_back(CoinCandidate(NICKEL_WEIGHT, NICKEL_DIAMETER, NICKEL_THICKNESS));
+	}
 }
